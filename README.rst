8df17beca33b1bd5d8c6b9b9faeaa83a5485873f
ratzeni <rossano.atzeni@crs4.it>
2018-03-14 15:39:34 +0100

    Ready to pip

---
+++ README.rst
@@ -0,0 +1,13 @@
+BikaClient
+======
+client package for Bika Open Source LIMS project
+
+
+Installation
+------------
+
+You can install the latest stable version from PyPI
+
+.. code:: console
+
+   $ pip install nglimsclient
--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
 import os
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'VERSION')) as f:
     __version__ = f.read().strip()
 
-extra_files = [os.path.join(here, 'VERSION')]
+with open(os.path.join(here, 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+with open(os.path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+extra_files = [os.path.join(here, 'requirements.txt'),
+               os.path.join(here, 'README.rst'),
+               os.path.join(here, 'VERSION'),
+               ]
 
 AuthorInfo = (
     ("Atzeni Rossano", "ratzeni@crs4.it"),
+    ("Gianmauro Cuccuru", "gmauro@crs4.it"),
 )
 
 setup(name="bikaclient",
       version=__version__,
       description="client package for bika lims",
+      long_description=long_description,
       author=",".join(a[0] for a in AuthorInfo),
       author_email=",".join("<%s>" % a[1] for a in AuthorInfo),
-      install_requires=['six'],
-      packages=['bikaclient'],
-      include_package_data=True,
+      zip_safe=True,
+      url='https://github.com/ratzeni/bika.client',
+      packages=find_packages(exclude=('tests',)),
+      keywords='utilities',
+      install_requires=required,
       package_data={'': extra_files},
       license='MIT',
       platforms="Posix; MacOS X; Windows",
